85451753c709755e5b20eef0ae6d28900058dca5
David <david.jiang1337@gmail.com>
2025-09-05 14:41:48 -0700

    forgot that 'this' keyword returns a pointer. All '.'s in RGBAPixel.cpp are replaced with '->'

--- lab_intro/cs221util/RGBAPixel.cpp
+++ lab_intro/cs221util/RGBAPixel.cpp
@@ -3,24 +3,24 @@
 namespace cs221util {
   
   RGBAPixel::RGBAPixel() {
-    this.r = 255;
-    this.g = 255;
-    this.b = 255;
-    this.a = 1;
+    this->r = 255;
+    this->g = 255;
+    this->b = 255;
+    this->a = 1;
   }
 
   RGBAPixel::RGBAPixel(const unsigned char r, const unsigned char g, const unsigned char b) {
-    this.r = r;
-    this.g = g;
-    this.b = b;
-    this.a = 1;
+    this->r = r;
+    this->g = g;
+    this->b = b;
+    this->a = 1;
   }
 
   RGBAPixel::RGBAPixel(const unsigned char r, const unsigned char g, const unsigned char b) {
-    this.r = r;
-    this.g = g;
-    this.b = b;
-    this.a = a;
+    this->r = r;
+    this->g = g;
+    this->b = b;
+    this->a = a;
   }
 
 }
